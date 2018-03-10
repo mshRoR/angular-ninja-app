dc6601586469e8c2c69330b83d3fb51fff0c1424
SHAJED <shajed_msh@yahoo.com>
2018-03-11 02:38:54 +0600

    top header menu convert directive to component

---
+++ app/header/top-header.component.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  angular.module('ninjaApp')
+    .component('topHeader', {
+      templateUrl: 'app/header/top-header.html'
+    });
+})();
--- app/header/top-header.directive.js
+++
@@ -1,7 +0,0 @@
-angular.module('ninjaApp')
-  .directive('topHeader', function(){
-    return {
-      scope: true,
-      templateUrl: 'app/header/top-header.html'
-    };
-  });
